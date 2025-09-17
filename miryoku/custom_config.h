5e668d3729fc68167517797764a99cf246708e5e
Dima Kolibabchuk <dmitry@kolibabchuk.com>
2025-09-17 14:39:52 +0300

    lets pray again

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,9 +1,12 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-// Custom Extra layer (QWERTY) with Russian support - includes missing [];
+// Custom Extra layer (QWERTY) with Russian support - uses special hallie mapping
 #define MIRYOKU_LAYER_EXTRA \
-&kp BSLH,  &kp Q,             &kp W,         &kp E,              &kp R,              &kp T,              &kp Y,             &kp U,             &kp I,            &kp O,             &kp P,                 &kp RBKT, \
-&kp CAPS,  U_MT(LGUI, A),     U_MT(LALT, S), U_MT(LCTRL, D),     U_MT(LSHFT, F),     &kp G,              &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),   U_MT(LALT, L),     U_MT(LGUI, SEMI),      &kp SQT, \
-&kp LSHFT, U_LT(U_BUTTON, Z), U_MT(RALT, X), &kp C,              &kp V,              &kp B,              &kp N,             &kp M,             &kp COMMA,        U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH), &kp LBKT, \
-U_NP      U_NP,              U_NP,          U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL), U_NP,              U_NP,                  U_NP
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SEMI),  \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH), \
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
+// Override mapping for Extra layer only
+#define MIRYOKU_MAPPING_EXTRA MIRYOKU_LAYOUTMAPPING_HALLIE_EXTRA
--- miryoku/mapping/42/hallie.h
+++ miryoku/mapping/42/hallie.h
@@ -32,6 +32,18 @@
 #define MIRYOKU_KLUDGE_SENSOR_LEFT 0
 #define MIRYOKU_KLUDGE_SENSOR_RIGHT 1
 
+// Extra layer mapping with Cyrillic support keys in outer columns
+#define MIRYOKU_LAYOUTMAPPING_HALLIE_EXTRA( \
+     K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
+) \
+&kp BSLH  K00  K01  K02  K03  K04   K05  K06  K07  K08  K09 &kp RBKT \
+&kp CAPS  K10  K11  K12  K13  K14   K15  K16  K17  K18  K19 &kp SQT \
+&kp LSHFT K20  K21  K22  K23  K24   K25  K26  K27  K28  K29 &kp LBKT \
+                  K32  K33  K34   K35  K36  K37
+
 #endif
 
 #define MIRYOKU_MAPPING MIRYOKU_LAYOUTMAPPING_HALLIE
