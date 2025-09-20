90092d260b6b8a18b9fb90ac7be168866a11e53e
Dima Kolibabchuk <dmitry@kolibabchuk.com>
2025-09-20 03:18:59 +0300

    fixes

--- miryoku/mapping/42/hallie.h
+++ miryoku/mapping/42/hallie.h
@@ -12,10 +12,10 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&td_bslh_esc     K00  K01  K02  K03  K04   K05  K06  K07  K08  K09 &kp BSPC \
-&u_mt HYPR COMMA K10  K11  K12  K13  K14   K15  K16  K17  K18  K19 &u_mt HYPR ENTER \
-&u_mt LSHFT DOT  K20  K21  K22  K23  K24   K25  K26  K27  K28  K29 &u_mt RSHFT EXCL \
-                           K32  K33  K34   K35  K36   K37
+&td_bslh_esc        K00  K01  K02  K03  K04   K05  K06  K07  K08  K09 &kp BSPC \
+&u_mt HYPR KP_COMMA K10  K11  K12  K13  K14   K15  K16  K17  K18  K19 &u_mt HYPR QMARK \
+&u_mt LSHFT KP_DOT  K20  K21  K22  K23  K24   K25  K26  K27  K28  K29 &u_mt RSHFT EXCL \
+                              K32  K33  K34   K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 0 1
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
@@ -40,10 +40,10 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&td_bslh_esc     K00  K01  K02  K03  K04   K05  K06  K07  K08  K09 &kp LBKT \
-&u_mt HYPR COMMA K10  K11  K12  K13  K14   K15  K16  K17  K18  K19 &u_mt HYPR SQT \
-&u_mt LSHFT DOT  K20  K21  K22  K23  K24   K25  K26  K27  K28  K29 &u_mt RSHFT RBKT \
-                           K32  K33  K34   K35  K36  K37
+&td_bslh_esc        K00  K01  K02  K03  K04   K05  K06  K07  K08  K09 &kp LBKT \
+&u_mt HYPR KP_COMMA K10  K11  K12  K13  K14   K15  K16  K17  K18  K19 &u_mt HYPR SQT \
+&u_mt LSHFT KP_DOT  K20  K21  K22  K23  K24   K25  K26  K27  K28  K29 &u_mt RSHFT RBKT \
+                              K32  K33  K34   K35  K36  K37
 
 #endif
 
