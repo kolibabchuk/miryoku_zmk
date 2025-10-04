2644c04e5ef155046c7de459c4252eea94cfd3fd
Dima Kolibabchuk <dmitry@kolibabchuk.com>
2025-10-04 23:53:14 +0300

    change custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,4 +2,3 @@
 // https://github.com/manna-harbour/miryoku
 
 // Override layer mapping for Extra layer - Colemak will be set via build config
-#define MIRYOKU_LAYERMAPPING_EXTRA MIRYOKU_LAYOUTMAPPING_HALLIE_BASE
