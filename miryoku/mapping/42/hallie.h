1d1c3e92307a9a24e67dc037834badfc6d272e61
Dima Kolibabchuk <dmitry@kolibabchuk.com>
2025-10-06 22:31:56 +0300

    customization

--- miryoku/mapping/42/hallie.h
+++ miryoku/mapping/42/hallie.h
@@ -12,8 +12,8 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&td_rbkt_esc     K00  K01  K02  K03  K04   K05  K06  K07  K08  K09 &td_lbkt_esc \
-&td_bslh_caps     K10  K11  K12  K13  K14   K15  K16  K17  K18  K19 &td_sqt_caps \
+&td_bslh_esc     K00  K01  K02  K03  K04   K05  K06  K07  K08  K09 &td_lbkt_esc \
+&td_rbkt_enter     K10  K11  K12  K13  K14   K15  K16  K17  K18  K19 &td_sqt_dot \
 &td_fslh_qmark    K20  K21  K22  K23  K24   K25  K26  K27  K28  K29 &td_minus_excl \
                               K32  K33  K34   K35  K36  K37
 
