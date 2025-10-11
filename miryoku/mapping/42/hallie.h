b7f1bd5ba2d993f6b0d6b61935afcdbc0a4ae541
Dima Kolibabchuk <dmitry@kolibabchuk.com>
2025-10-12 02:58:05 +0300

    kovbasa

--- miryoku/mapping/42/hallie.h
+++ miryoku/mapping/42/hallie.h
@@ -15,7 +15,7 @@
 &td_bslh_globe_base  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  U_MT(GLOBE, LBKT) \
 U_MT(HYPR, RBKT)     K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  U_MT(HYPR, SQT) \
 U_MT(LSHFT, ESC)     K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  U_MT(RSHFT, ESC) \
-                             K32  K33  K34    K35  K36  K37
+                               K32  K33  K34    K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 0 1
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
