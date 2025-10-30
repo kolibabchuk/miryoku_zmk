584a4d9fbdc549f12e6a20770073b7e05900ad18
Dima Kolibabchuk <dmitry@kolibabchuk.com>
2025-10-30 22:16:16 +0200

    add unicode behaviours

--- miryoku/mapping/42/hallie.h
+++ miryoku/mapping/42/hallie.h
@@ -6,6 +6,49 @@
 #define XXX &none
 #define HYPR LG(LA(LS(LCTRL)))
 
+/ {
+    macros {
+        // Unicode macro for tilde ~ (U+007E) using macOS Unicode Hex Input
+        uc_tilde: uc_tilde {
+            compatible = "zmk,behavior-macro";
+            label = "UC_TILDE";
+            #binding-cells = <0>;
+            wait-ms = <10>;
+            tap-ms = <10>;
+            bindings
+                = <&macro_press &kp LALT>
+                , <&macro_tap &kp N0 &kp N0 &kp N7 &kp E>
+                , <&macro_release &kp LALT>
+                ;
+        };
+
+        // Unicode macro for backtick ` (U+0060) using macOS Unicode Hex Input
+        uc_backtick: uc_backtick {
+            compatible = "zmk,behavior-macro";
+            label = "UC_BACKTICK";
+            #binding-cells = <0>;
+            wait-ms = <10>;
+            tap-ms = <10>;
+            bindings
+                = <&macro_press &kp LALT>
+                , <&macro_tap &kp N0 &kp N0 &kp N6 &kp N0>
+                , <&macro_release &kp LALT>
+                ;
+        };
+    };
+
+    behaviors {
+        // Mod-morph: tap = tilde, shift+tap = backtick
+        mm_tilde_backtick: mm_tilde_backtick {
+            compatible = "zmk,behavior-mod-morph";
+            label = "MM_TILDE_BACKTICK";
+            #binding-cells = <0>;
+            bindings = <&uc_tilde>, <&uc_backtick>;
+            mods = <(MOD_LSFT|MOD_RSFT)>;
+        };
+    };
+};
+
 // Default mapping for BASE, EXTRA, and TAP layers
 #define MIRYOKU_LAYOUTMAPPING_HALLIE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
@@ -18,76 +61,76 @@ U_MT(HYPR, RBKT)     K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  U_MT(HY
 U_MT(LSHFT, ESC)     K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  U_MT(RSHFT, ESC) \
                                K32  K33  K34    K35  K36  K37
 
-// BUTTON layer mapping with numbers 1-6 on outer columns
+// BUTTON layer mapping with custom outer column keys
 #define MIRYOKU_LAYOUTMAPPING_BUTTON( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp N1  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp N4 \
-&kp N2  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp N5 \
-&kp N3  K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp N6 \
+&kp ESC   K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp GLOBE \
+&kp HYPR  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp ENTER \
+&kp GLOBE K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp RSHFT \
                      K32  K33  K34    K35  K36  K37
 
-// NAV layer mapping with numbers 1-6 on outer columns
+// NAV layer mapping with custom outer column keys
 #define MIRYOKU_LAYOUTMAPPING_NAV( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp N1  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp N4 \
-&kp N2  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp N5 \
-&kp N3  K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp N6 \
+&kp ESC   K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp GLOBE \
+&kp HYPR  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp ENTER \
+&kp GLOBE K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp RSHFT \
                      K32  K33  K34    K35  K36  K37
 
-// MOUSE layer mapping with numbers 1-6 on outer columns
+// MOUSE layer mapping with custom outer column keys
 #define MIRYOKU_LAYOUTMAPPING_MOUSE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp N1  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp N4 \
-&kp N2  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp N5 \
-&kp N3  K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp N6 \
+&kp ESC   K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp GLOBE \
+&kp HYPR  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp ENTER \
+&kp GLOBE K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp RSHFT \
                      K32  K33  K34    K35  K36  K37
 
-// MEDIA layer mapping with numbers 1-6 on outer columns
+// MEDIA layer mapping with custom outer column keys
 #define MIRYOKU_LAYOUTMAPPING_MEDIA( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp N1  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp N4 \
-&kp N2  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp N5 \
-&kp N3  K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp N6 \
+&kp ESC   K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp GLOBE \
+&kp HYPR  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp ENTER \
+&kp GLOBE K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp RSHFT \
                      K32  K33  K34    K35  K36  K37
 
-// NUM layer mapping with numbers 1-6 on outer columns
+// NUM layer mapping with custom outer column keys
 #define MIRYOKU_LAYOUTMAPPING_NUM( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp N1  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp N4 \
-&kp N2  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp N5 \
-&kp N3  K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp N6 \
+&kp ESC   K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp GLOBE \
+&kp HYPR  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp ENTER \
+&kp GLOBE K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp RSHFT \
                      K32  K33  K34    K35  K36  K37
 
-// SYM layer mapping with numbers 1-6 on outer columns
+// SYM layer mapping with custom outer column keys and unicode tilde/backtick
 #define MIRYOKU_LAYOUTMAPPING_SYM( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp N1  K00  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp N4 \
-&kp N2  K10  K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp N5 \
-&kp N3  K20  K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp N6 \
+&kp N1  &mm_tilde_backtick  K01  K02  K03  K04    K05  K06  K07  K08  K09  &kp N4 \
+&kp N2  K10                 K11  K12  K13  K14    K15  K16  K17  K18  K19  &kp N5 \
+&kp N3  K20                 K21  K22  K23  K24    K25  K26  K27  K28  K29  &kp N6 \
                      K32  K33  K34    K35  K36  K37
 
 // FUN layer mapping with numbers 1-6 on outer columns
